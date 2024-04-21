fdcc8ee72535464ea7143506b94898965a94397b
Denys iElkin <hvoarang@gmail.com>
2024-04-22 01:53:27 +0300

    asd

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,34 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+// {
+//     src: '../images/gocorona.jpg',
+//     title: 'goCorona',
+//     copy: 'My first practic',
+//   },
+//   {
+//     src: './images/cocktail.jpg',
+//     title: 'Coctails',
+//     copy: 'Team project. Used JS',
+//   },
+//   {
+//     src: '../images/icecream.jpg',
+//     title: 'IceCream',
+//     copy: 'Team project. Used JS',
+//   },
+//   {
+//     src: '../images/phonebook.jpg',
+//     title: 'PhoneBook',
+//     copy: 'My first React practic',
+//   },
+//   {
+//     src: './images/webstudio.jpg',
+//     title: 'WebStudio',
+//     copy: 'HTML/CSS/SASS/JS',
+//   },
+//   {
+//     src: './images/VR.jpg',
+//     title: 'VR',
+//     copy: 'HTML/CSS/SASS',
+//   },
