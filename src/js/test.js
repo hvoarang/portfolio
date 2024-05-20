652a5b5884c4f6c534eb2158d658f431271fb34d
Denys iElkin <hvoarang@gmail.com>
2024-05-21 01:59:11 +0300

    new fix

--- src/js/test.js
+++ src/js/test.js
@@ -65,3 +65,23 @@ console.log(global); // global
 console.log(blockA); // ReferenceError: blockA is not defined
 console.log(blockB); // ReferenceError: blockB is not defined
 console.log(blockC); // ReferenceError: blockC is not defined
+
+let namea = 'pashaaaaa';
+console.log(namea.slice(0, 2));
+
+let password = '';
+
+do {
+  password = prompt('Введіть пароль довший 4-х символів', '');
+} while (password.length < 5);
+
+console.log('Ввели пароль: ', password);
+
+const target = 3;
+let sum = 0;
+
+for (let i = 0; i <= target; i += 1) {
+  sum += i;
+}
+
+console.log(sum);
