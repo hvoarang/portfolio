8f2a5551773b09b07c86ab9c668f42b9fe88fe59
Denys iElkin <hvoarang@gmail.com>
2024-07-16 08:35:09 +0300

    new fix

--- src/js/js_hw_task_module_five.js
+++ src/js/js_hw_task_module_five.js
@@ -777,4 +777,3 @@ mango.blacklist("poly@mail.com");
 console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 console.log(mango.isBlacklisted("mango@mail.com")); // false
 console.log(mango.isBlacklisted("poly@mail.com")); // true
-console.log(mango.isBlacklisted("poly@mail.com")); // true
