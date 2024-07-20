f11d716a72b4f2eb475e578fca8365534e973c45
Denys iElkin <hvoarang@gmail.com>
2024-07-20 09:29:02 +0300

    new fix

--- src/js/js_hw_task_module_five.js
+++ src/js/js_hw_task_module_five.js
@@ -817,3 +817,24 @@ const bmw = new Car({ price: 64000 });
 console.log(Car.checkPrice(audi.price));
 console.log(Car.checkPrice(bmw.price));
 console.log(Car.checkPrice(bmw.price));
+
+
+const image = document.querySelector(".image");
+
+console.log(image.attributes); // NamedNodeMap {0: class, 1: src, 2: alt, length: 3}
+
+console.log(image.hasAttribute("src")); // true
+
+console.log(image.getAttribute("alt")); // "Rocks and waterfall"
+
+image.setAttribute("alt", "Amazing nature");
+
+console.log(image.getAttribute("alt")); // Amazing nature
+
+
+
+const saveBtn = document.querySelector('.editor button[data-action="save"]');
+const closeBtn = document.querySelector('.editor button[data-action="close"]');
+
+console.log(saveBtn.dataset.action); //save
+console.log(closeBtn.dataset.action); //close
