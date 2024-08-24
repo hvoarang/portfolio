9ef58784e87446f3bd8f914a13c19b5c9a1b9130
Denys iElkin <hvoarang@gmail.com>
2024-08-25 02:04:41 +0300

    new fix

--- src/js/test.js
+++ src/js/test.js
@@ -1,55 +1,12 @@
-const colorPalette = document.querySelector('.color-palette');
-const output = document.querySelector('.output');
+// Изменить значение переменной isSuccess для вызова разрешения или отклонения
+const isSuccess = true;
 
-colorPalette.addEventListener('click', selectColor);
-
-// This is where delegation «magic» happens
-function selectColor(event) {
-  if (event.target.nodeName !== 'BUTTON') {
-    return;
-  }
-
-  const selectedColor = event.target.dataset.color;
-  output.textContent = `Selected color: ${selectedColor}`;
-  output.style.color = selectedColor;
-}
-
-// Some helper functions to render palette items
-createPaletteItems();
-
-function createPaletteItems() {
-  const items = [];
-  for (let i = 0; i < 60; i++) {
-    const color = getRandomHexColor();
-    const item = document.createElement('button');
-    item.type = 'button';
-    item.dataset.color = color;
-    item.style.backgroundColor = color;
-    item.classList.add('item');
-    items.push(item);
-  }
-  colorPalette.append(...items);
-}
-
-function getRandomHexColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-
-  return;
-}
-
-const obj = {
-  value: 42,
-  getValue: function () {
-    const inner = () => this.value;
-    return inner();
-  },
-};
-console.log(obj.getValue());
-
-let { length } = 5;
-console.log(length);
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve('Успех! Значение передано в функцию resolve');
+    } else {
+      reject('Ошибка! Ошибка передана в функцию reject');
+    }
+  }, 2000);
+});
