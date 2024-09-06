396f135e7953185ac75e1f730bb0eadfcbbfac79
hvoarang <hvoarang@gmail.com>
2024-09-06 07:35:53 +0000

    Deploying to gh-pages from @ hvoarang/portfolio@bf74b456fcfd944542e1b660aeb4ec945d73d90b 🚀

---
+++ index.f233b2ac.js
@@ -0,0 +1,2 @@
+let e;(e="petya",new Promise((o,c)=>{console.log(`Fetching data for ${e}`),setTimeout(()=>{o("success value")},2e3)})).then(e=>console.log(e)).catch(e=>console.error(e));
+//# sourceMappingURL=index.f233b2ac.js.map
